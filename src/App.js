e21a6932b031bcd6c7bc5dd46c0a58e6596ed061
TanjilulAnwar <tanjilulanwar@gmail.com>
2020-10-15 13:10:04 +0600

    different styling approach

--- src/App.js
+++ src/App.js
@@ -11,14 +11,22 @@ import './App.css';
 // import EventBind from './components/EventBind';
 // import ParentComponent from './components/ParentComponent';
 // import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList'
+//import NameList from './components/NameList'
+//import StyleSheet from './components/Stylesheet'
+import Inline from './components/Inline'
+import './appStyles.css'
+import styles from './appStyles.module.css'
 class App extends Component{
 
   render(){
 
     return (
       <div className="App">
-      <NameList/>
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      {/* <StyleSheet primary= {true}/> */}
+      <Inline/>
+      {/* <NameList/> */}
       {/* <UserGreeting/> */}
       {/* <ParentComponent/> */}
         {/* <EventBind/> */}
